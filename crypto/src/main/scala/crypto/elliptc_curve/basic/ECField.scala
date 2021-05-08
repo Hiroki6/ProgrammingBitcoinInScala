bf6285820615e7feb6af04d5863ac8b9d380c628
Hiroki6 <hirokifujino0108@gmail.com>
2021-05-08 21:21:26 +0200

    delete example code

--- crypto/src/main/scala/crypto/elliptc_curve/basic/ECField.scala
+++ crypto/src/main/scala/crypto/elliptc_curve/basic/ECField.scala
@@ -15,31 +15,3 @@ object ECField {
     }
   }
 }
-
-object ECFieldSample {
-  def main(args: Array[String]): Unit = {
-    initialization()
-  }
-
-  def initialization(): Unit = {
-    val a = ECField(7, 13)
-    val b = ECField(12, 13)
-    println(a == b)
-    println(a == a)
-  }
-
-  def additional(): Unit = {
-    val a = ECField(7, 13)
-    val b = ECField(12, 13)
-    val c = ECField(6, 13)
-    println(a + b)
-    println(a + b == c)
-  }
-
-  def pow(): Unit = {
-    val a = ECField(7, 13)
-    val b = ECField(8, 13)
-    val powed = a ** -3
-    println(powed == b)
-  }
-}
--- crypto/src/main/scala/crypto/elliptc_curve/basic/ECPoint.scala
+++ crypto/src/main/scala/crypto/elliptc_curve/basic/ECPoint.scala
@@ -2,8 +2,6 @@ package crypto.elliptc_curve.basic
 
 import crypto.elliptc_curve.{Point, PointException}
 
-import scala.util.{Failure, Success, Try}
-
 case class ECPoint(x: ECField, y: ECField, a: ECField, b: ECField)
     extends Point[ECField, ECPoint] {
   override def create(x: ECField, y: ECField, a: ECField, b: ECField): ECPoint =
@@ -18,45 +16,5 @@ object ECPoint {
       }
     }
     new ECPoint(x, y, a, b) {}
-  }
-}
-
-object ECPointSample {
-  def main(args: Array[String]): Unit = {
-    Try(test2()) match {
-      case Success(point) => println(point)
-      case Failure(ex) => println(ex.getMessage)
-    }
-    test3()
-  }
-
-  def test1() = {
-    val a = ECField(0, 223)
-    val b = ECField(7, 223)
-    val x = ECField(192, 223)
-    val y = ECField(105, 223)
-    ECPoint(x, y, a, b)
-  }
-
-  def test2() = {
-    val a = ECField(0, 223)
-    val b = ECField(7, 223)
-    val x1 = ECField(143, 223)
-    val y1 = ECField(98, 223)
-    val x2 = ECField(76, 223)
-    val y2 = ECField(66, 223)
-    val p1 = ECPoint(x1, y1, a, b)
-    val p2 = ECPoint(x2, y2, a, b)
-    p1 + p2
-  }
-
-  def test3() = {
-    val a = ECField(0, 223)
-    val b = ECField(7, 223)
-    val x1 = ECField(143, 223)
-    val y1 = ECField(98, 223)
-    val p1 = ECPoint(x1, y1, a, b)
-    assert((p1 + p1) == p1 * 2)
-    assert((p1 * 2 + p1) == p1 * 3)
   }
 }
